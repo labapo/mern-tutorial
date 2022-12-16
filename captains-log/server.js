7bc06aec67b91dbae4098333ce0e1c6340bbc36f
Lorivie Abapo <lorivieabapo@Lorivies-MacBook-Pro.local>
2022-12-16 09:30:27 -0700

    first commit

---
+++ captains-log/server.js
@@ -0,0 +1,51 @@
+//////////SET UP ENVIRONMENT
+
+
+
+
+
+
+
+
+
+
+
+
+//////////OTHER REQUIRES
+///database connections
+/// get rid of depreciate warnings
+///method override
+
+
+
+
+
+//////////ESTABLISH CONNECTION WITH MONGO
+/// connect to mongoose
+/// remove deprication warnings
+/// events for when connection opens
+
+
+
+
+
+//////////MIDDLEWARE
+///method override middleware
+///url encoded to get access to the body 
+
+
+
+
+//////////ROUTES
+
+
+
+
+
+///index-GET
+///new - GET
+///create - POST
+///show - GET
+///edit - GET
+///update - PUT
+///destroy - DELETE
